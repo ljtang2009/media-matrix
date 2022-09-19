3d191028122920cf791fa4c2c6a559f223029159
Tang <ho_ho_gl@hotmail.com>
2022-09-20 04:03:26 +0800

    添加stylelint

--- build/webpack.config.prod.ts
+++ build/webpack.config.prod.ts
@@ -5,6 +5,7 @@ import { merge } from 'webpack-merge';
 import ESLintPlugin from 'eslint-webpack-plugin';
 import { analyzerPort } from '../config';
 import getPort from 'get-port';
+import StylelintPlugin from 'stylelint-webpack-plugin';
 
 async function getConfig() {
   return merge(webpackBaseConfig, {
@@ -17,6 +18,9 @@ async function getConfig() {
       new ProgressPlugin(),
       new ESLintPlugin({
         extensions: ['vue', 'js', 'jsx', 'cjs', 'mjs', 'ts', 'tsx', 'cts', 'mts'],
+      }),
+      new StylelintPlugin({
+        extensions: ['css', 'less', 'scss', 'sass'],
       }),
     ],
   });
